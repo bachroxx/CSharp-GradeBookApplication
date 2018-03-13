3603d206f4421c120dcbe4328cbd2580e5d8b717
bachroxx <bachroxx@cox.net>
2018-03-13 13:51:02 -0400

    added ranks
    added ranks

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,6 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -8,5 +10,23 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+            public override char GetLetterGrade(double averageGrade)
+            {
+                if (Students.Count < 5)
+                    throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+
+                var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+                var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            return base.GetLetterGrade(averageGrade);
+            }
     }
 }
